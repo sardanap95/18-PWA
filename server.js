41f9195cfacab49a7f71f5591937aae7550058c4
Puspi <sardanap95@gmail.com>
2020-11-29 06:45:18 +0530

    Cors added.

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 const PORT = process.env.PORT || 3000;
+var cors = require("cors");
 
 const app = express();
-
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
